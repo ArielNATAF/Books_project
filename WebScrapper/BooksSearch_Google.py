0f53d1623296007b4a3b4260e2d9f0ad48b44db1
Carine Therond <carine.raynaud@gmx.fr>
2021-03-14 20:54:51 +0100

    Last columns update

--- WebScrapper/BooksSearch_Google.py
+++ WebScrapper/BooksSearch_Google.py
@@ -1,10 +1,12 @@
 import sys
-cheminMonGitHub = 'D:\DocsDeCara\Boulot\IA_ML\MonGitHub\repBooksProjet\Books_project/'
-cheminMonGitHub = cheminMonGitHub.replace('\\', '/')
+cheminMonGitHub = 'D:/DocsDeCara/Boulot/IA_ML/MonGitHub/repBooksProjet/Books_project/WebScrapper/'
 sys.path.append(cheminMonGitHub)
+    
+import Parser_csv_BooksList as P
     
 import pandas as pd
 import time
+import re
 
 
 def GoogleSearchFunction(isbn = None, title = None, author = None):
@@ -25,11 +27,13 @@ def GoogleSearchFunction(isbn = None, title = None, author = None):
     pubData = ''
     catSearched = ''
     descSearched = ''
+    langSearched = ''
+    imageSearched = ''
+    pageSearched = 0
 
     ISBNfound = False
     AUTHORfound = False
-    repTitleGoogle = ''
-    repIsbnGoogle = ''
+    repGoogle = ''
     
     try:
         #book is searched on internet thanks to its ISBN_10 identifier
@@ -44,11 +48,11 @@ def GoogleSearchFunction(isbn = None, title = None, author = None):
     
             try:
                 #In case where a book reference has been found
-                repIsbnGoogle = pd.read_json(requeteIsbnGoogle)
+                repGoogle = pd.read_json(requeteIsbnGoogle)
                 ISBNfound = True
             except:
                 #In case where a book reference has NOT been found                
-                repIsbnGoogle = pd.read_json(requeteIsbnGoogle, typ = 'series')
+                repGoogle = pd.read_json(requeteIsbnGoogle, typ = 'series')
                 ISBNfound = False
             
         
@@ -65,108 +69,117 @@ def GoogleSearchFunction(isbn = None, title = None, author = None):
                 #=> We must take care of ' (replace('\'', '')) and space (replace(' ','%20'))
                 requeteTitleGoogle = "https://www.googleapis.com/books/v1/volumes?q=title:%s" % (titleSearched.replace('\'', '').replace(' ','%20'))
 #                print(requeteTitleGoogle)
-                repTitleGoogle = pd.read_json(requeteTitleGoogle) 
+                repGoogle = pd.read_json(requeteTitleGoogle) 
                 
                 #The Title has been found on Google Books
                 #We only analyse the first book's reference retrieved => repTitleGoogle['items'][0]
-                if (repTitleGoogle['totalItems'][0] != 1):
+                if (repGoogle['totalItems'][0] != 1):
     
-                    if ('authors' in repTitleGoogle['items'][0]['volumeInfo']):
+                    if ('authors' in repGoogle['items'][0]['volumeInfo']):
                         
                         #All authors proposed by Google
-                        autGoogle = repTitleGoogle['items'][0]['volumeInfo']['authors']
+                        autGoogle = repGoogle['items'][0]['volumeInfo']['authors']
                         
                         #Among all authors proposed by Google, we search correspondance with the first provided author
                         j = 0
                         while ((j < len(autGoogle)) and (AUTHORfound == False)):
                             
                             if (autSearchedInternal in autGoogle[j].lower()):
-                                AUTHORfound = True    
+                                AUTHORfound = True  
+                                ISBNfound = True
                                 
                             j += 1
 
-
         #ISBN or title/author has been recognized on Google Books
-        if ((ISBNfound == True) or (AUTHORfound == True)):
-            titleSearched = repIsbnGoogle['items'][0]['volumeInfo']['title']
+        if (ISBNfound == True):
+            titleSearched = repGoogle['items'][0]['volumeInfo']['title']
             
-            if 'subtitle' in repIsbnGoogle['items'][0]['volumeInfo']:
-                titleSearched = titleSearched + ' : ' + repIsbnGoogle['items'][0]['volumeInfo']['subtitle']
-    
+            if 'subtitle' in repGoogle['items'][0]['volumeInfo']:
+                titleSearched = titleSearched + ' : ' + repGoogle['items'][0]['volumeInfo']['subtitle']
             #'industryIdentifiers' stands for ISBN_10, ISBN_13 or OtherID
-            if ('industryIdentifiers' in repIsbnGoogle['items'][0]['volumeInfo']):
+            if ('industryIdentifiers' in repGoogle['items'][0]['volumeInfo']):
                 
                 #Among all identifiers type proposed by Google, we search "ISBN_10", "ISBN_13" or "OtherID"
-                for k in range(len(repIsbnGoogle['items'][0]['volumeInfo']['industryIdentifiers'])):
+                for k in range(len(repGoogle['items'][0]['volumeInfo']['industryIdentifiers'])):
                     
-                    if repIsbnGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['type'] == "ISBN_10":
-                        ISBNdict["ISBN_10"] = repIsbnGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['identifier']
+                    if repGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['type'] == "ISBN_10":
+                        ISBNdict["ISBN_10"] = repGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['identifier']
                         ISBNfound = True
                         
-                    elif repIsbnGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['type'] == "ISBN_13":
-                        ISBNdict["ISBN_13"] = repIsbnGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['identifier']
+                    elif repGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['type'] == "ISBN_13":
+                        ISBNdict["ISBN_13"] = repGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['identifier']
                         ISBNfound = True
                         
                     else:
-                        ISBNdict["OtherID"] = repIsbnGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['identifier']
+                        ISBNdict["OtherID"] = repGoogle['items'][0]['volumeInfo']['industryIdentifiers'][k]['identifier']
+                        ISBNfound = True
+
+            if ('authors' in repGoogle['items'][0]['volumeInfo']):
+                autSearched = repGoogle['items'][0]['volumeInfo']['authors'][0]
                 
-            if ('authors' in repIsbnGoogle['items'][0]['volumeInfo']):
-                autSearched = repIsbnGoogle['items'][0]['volumeInfo']['authors'][0]
-                
-            if ('publisher' in repIsbnGoogle['items'][0]['volumeInfo']):
-                pubSearched = repIsbnGoogle['items'][0]['volumeInfo']['publisher']
+            if ('publisher' in repGoogle['items'][0]['volumeInfo']):
+                pubSearched = repGoogle['items'][0]['volumeInfo']['publisher']
                                     
-            if ('publishedDate' in repIsbnGoogle['items'][0]['volumeInfo']):
-                pubData = repIsbnGoogle['items'][0]['volumeInfo']['publishedDate']
+            if ('publishedDate' in repGoogle['items'][0]['volumeInfo']):
+                pubData = repGoogle['items'][0]['volumeInfo']['publishedDate']
+                pubData = re.search("[0-9]{4,4}", pubData)[0]
 
-            if ('categories' in repIsbnGoogle['items'][0]['volumeInfo']):
-                catSearched = repIsbnGoogle['items'][0]['volumeInfo']['categories'][0]
+            if ('categories' in repGoogle['items'][0]['volumeInfo']):
+                catSearched = repGoogle['items'][0]['volumeInfo']['categories'][0]
 
-            descSearched = repIsbnGoogle['items'][0]['searchInfo']['textSnippet']                    
+            if ('searchInfo' in repGoogle['items'][0]):
+                descSearched = repGoogle['items'][0]['searchInfo']['textSnippet']                    
 
+            if ('language' in repGoogle['items'][0]['volumeInfo']):
+                langSearched = repGoogle['items'][0]['volumeInfo']['language']
 
-        return ISBNfound, [ISBNdict["ISBN_10"], ISBNdict["ISBN_13"], ISBNdict["OtherID"], titleSearched, autSearched, \
-                           pubData, pubSearched, catSearched, descSearched]
+            if ('imageLinks' in repGoogle['items'][0]['volumeInfo']):
+                imageSearched = repGoogle['items'][0]['volumeInfo']['imageLinks']['smallThumbnail']
 
-    except Exception as excp:
-        raise Exception("There is un problem: ", excp)
+            if ('pageCount' in repGoogle['items'][0]['volumeInfo']):
+                pageSearched = repGoogle['items'][0]['volumeInfo']['pageCount']
+
+        return (ISBNfound, [ISBNdict["ISBN_10"], ISBNdict["ISBN_13"], ISBNdict["OtherID"], titleSearched, autSearched, \
+                           pubData, pubSearched, catSearched, descSearched, langSearched, imageSearched, pageSearched, '', '', ''])
+
+    except:
+        return (False, [ISBNdict["ISBN_10"], ISBNdict["ISBN_13"], ISBNdict["OtherID"], titleSearched, autSearched, \
+                           pubData, pubSearched, catSearched, descSearched, langSearched, imageSearched, pageSearched, '', '', ''])
 
 
 
 def GoogleSearch(f, SaveFileName, theColumns, ind_start, ind_end):
 
-    NotFoubndBooks = 0    
+    NotFoundBooks = 0    
     pdT = pd.DataFrame(columns = theColumns)
 
-    for i in range(0, ind_start): 
+    for i in range(0, ind_start-1): 
         f.readline()
         
-    csvLineParsed = ParserCsvInputBookCrossing(f.readline())
+    csvLineParsed = P.ParserCsvInputBookCrossing(f.readline())
     ret, refBook = GoogleSearchFunction(isbn = csvLineParsed[0])
     pdT.loc[0] = refBook
-    pd.DataFrame(pdT.loc[0:1], columns = theColumns).to_csv(SaveFileName, sep = ';', index = False, mode = 'w')
+    pdT.loc[0:1].to_csv(SaveFileName, sep = ';', index = False, mode = 'w')
 
-    savingRate = int(ind_end / 4)
     #i doesn't start at 0 so that the trigger index 'len(pdT) - savingRate:len(pdT)+1' of save is correct
-    for i in range(1, ind_end):    
-        csvLineParsed = ParserCsvInputBookCrossing(f.readline())
+    for i in range(ind_start+1, ind_end):    
+        csvLineParsed = P.ParserCsvInputBookCrossing(f.readline())
 
         ret = False
         ret, refBook = GoogleSearchFunction(isbn = csvLineParsed[0])
         
         if (ret == False):
             ret, refBook = GoogleSearchFunction(title = csvLineParsed[1], author = csvLineParsed[2].split(';')[-1])
-            
+        
         if (ret == False):
-            NotFoubndBooks += 1
+            NotFoundBooks += 1
 
-        pdT.loc[len(pdT)] = refBook
-        if (i % savingRate == 0):
-            pdT.loc[len(pdT) - savingRate:len(pdT)+1].to_csv(SaveFileName, sep = ';', index = False, mode = 'a', header = False)
-            #Google doesn't like when too much requests are performed...
-            time.sleep(10.0)
+        pdT.loc[0] = refBook
+        pdT.loc[0:1].to_csv(SaveFileName, sep = ';', index = False, mode = 'a', header = False)
+        #Google doesn't like when too much requests are performed...
+        #time.sleep(10.0)
     
-    return NotFoubndBooks
+    return NotFoundBooks
 
 
 
@@ -202,17 +215,20 @@ if __name__ == "__main__":
     
     cheminGoogleBooks = 'D:\DocsDeCara\Boulot\IA_ML\DSTI\Programme\ML_with_Python\Projet\Donnees\GoogleBooks/'
     cheminGoogleBooks = cheminGoogleBooks.replace('\\', '/')
-    SaveFileName = cheminGoogleBooks + 'GoggleBooks_InternetSearch_06_03_2021.csv'
-    googleColumns = ['ISBN_10', 'ISBN_13', 'OtherID', 'Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher', 'Category', 'Description']
+    SaveFileName = cheminGoogleBooks + 'GoogleBooks_InternetSearch_14_03_2021.csv'
+
+    theColumns = ['ISBN_10', 'ISBN_13', 'OtherID', 'Book-Title', 'Book-Author', \
+                        'Year-Of-Publication', 'Publisher', 'Category', 'Description', 'Language', \
+                        'Image', 'Pages', 'Awards', "Author's genre", 'Same serie']
 
     #List of books to search on Google
     f = open(cheminBookCrossing + 'Extrait1000_BX-Books.csv', encoding="utf8", errors="replace")
     f.readline()
     
     #Google search function
-    NotFoubndBooks = GoogleSearch(f, SaveFileName, googleColumns, 0, 10) 
+    NotFoundBooks = GoogleSearch(f, SaveFileName, theColumns, 0, 15) 
 
-    print("Not found books: %d" % NotFoubndBooks)
+    print("Not found books: %d" % NotFoundBooks)
     f.close()   
     
     
